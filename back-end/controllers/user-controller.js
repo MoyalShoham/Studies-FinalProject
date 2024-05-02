f0b35165626c6ba2b089e3b4817a80b8874fb734
shoham <moyal.shoham@gmail.com>
2024-05-02 08:32:00 +0300

    file upload

--- back-end/controllers/user-controller.js
+++ back-end/controllers/user-controller.js
@@ -2,6 +2,13 @@ const getUsers = async (req, res) => {
    return res.status(200).json({ message: 'Hello World!' });
 }
 
+const uploadFile = async (req, res) => {
+    console.log(req.body);
+    return res.status(200).json({ message: 'Hello World!' });
+}
+
 module.exports = {
-    getUsers
+    getUsers,
+    uploadFile
+    
 }
--- back-end/routes/user-routes.js
+++ back-end/routes/user-routes.js
@@ -1,11 +1,22 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+const upload = multer({ storage: storage});
 
 const userController = require('../controllers/user-controller');
 
 router.get('/', userController.getUsers);
 
-
+router.post('/api/upload', upload.single('file'), userController.uploadFile);
 // router.get('/', (req, res) => {
 //     res.send('Hello World!');
 // });
